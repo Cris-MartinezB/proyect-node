fbd80e0de3223fc4e83438038f48b00d29d1e699
Cris-MartinezB <text@test.com>
2023-03-28 17:31:17 +0200

    api node

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// https://www.youtube.com/watch?v=qFmwRriNJWs
+
+const express = require('express');
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/api/v1/workouts", v1WorkoutRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+});
